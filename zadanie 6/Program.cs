9826912c4c9bb65d21c2ccaaecec292295b6f733
KanamiCWL <pitosss123@gmail.com>
2025-03-07 14:02:30 +0100

    dokonczenie

---
+++ zadanie 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, suma, roznica, iloczyn, iloraz;
+            Console.WriteLine("podaj a= ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("podaj b= ");
+            b = Convert.ToDouble(Console.ReadLine());
+            suma = a + b;
+            roznica = a - b;
+            iloczyn = a * b;
+            iloraz = a / b;
+            Console.WriteLine("suma={0}", suma);
+            Console.WriteLine("roznica={0}", roznica);
+            Console.WriteLine("iloczyn={0}", iloczyn);
+            Console.WriteLine("iloraz={0}", iloraz);
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ zadanie 7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący dwie liczby całkowite i wypisujący na ekranie iloraz tych liczb z uwzględnieniem części całkowitej i reszty z dzielenia.
+            int a, b, calkowita, reszta;
+            Console.WriteLine("podaj a= ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("podaj b= ");
+            b = Convert.ToInt32(Console.ReadLine());
+            calkowita = a / b;
+            reszta = a % b;
+            Console.WriteLine("calkowita={0}", calkowita);
+            Console.WriteLine("reszta={0}", reszta);
+            Console.ReadKey(true);
+
+        }
+    }
+}
